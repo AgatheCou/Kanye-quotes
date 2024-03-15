ce42954bc60c2074b0a7de19249cf6699f0df1f7
Coulombier Agathe <Coulombier.Agathe1@DEME-GROUP.COM>
2024-03-15 11:25:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+let content;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {content});
+  });
+
+app.get("/get-quote", async (req, res) => {
+try {
+    const response = await axios.get("https://api.kanye.rest");
+    const result = response.data;
+    console.log(result)
+    res.render("index.ejs", { content: {quote : result.quote, label: "Another one" }});
+} catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+    error: error.message,
+    });
+}
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
